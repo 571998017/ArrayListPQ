0ae925d4dd522e1e4122f4ea4863ba09cca737a7
Pasit Somkana <Pasit_Somkana@crru.ac.th>
2015-10-09 13:12:09 +0700

    Update PQ

---
+++ TestArrayListPQ.java
@@ -0,0 +1,19 @@
+public class TestArrayListPQ {
+
+public static void main(String[] args) {
+
+ArrayListPQ pq = new ArrayListPQ();
+
+
+
+pq.enQueue(new Integer(3));
+
+pq.enQueue(new Integer(9));
+
+
+
+System.out.println(pq.deQueue());
+
+}
+
+}
